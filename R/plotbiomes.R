655936ccab832b9509b0fb87a59cce301c969783
valentinitnelav <stefan.valentin87@yahoo.com>
2017-11-12 17:37:55 +0100

    add package documentation

---
+++ R/plotbiomes.R
@@ -0,0 +1,32 @@
+###############################################################################
+## This file documents the package.
+###############################################################################
+
+#' @title
+#' plotbiomes: Data for plotting Whittaker biomes with ggplot2.
+#'
+#' @description
+#' This is a package without functions, containing only data for plotting Whittaker biomes with ggplot2.
+#'
+#' The original graph is Figure 5.5 in *Ricklefs, R. E. (2008), The economy of nature. W. H. Freeman and Company.*
+#' (Chapter 5, Biological Communities, The biome concept).
+#' The figure was processed and brought into an R friendly format.
+#' Details are given in [Whittaker_biomes_dataset](https://rawgit.com/valentinitnelav/plotbiomes/master/inst/doc/Whittaker_biomes_dataset.html)
+#' vignette, or run `RShowDoc("Whittaker_biomes_dataset", package = "plotbiomes")`
+#'
+#' @section Help:
+#' Examples at: [Whittaker_biomes_examples](https://rawgit.com/valentinitnelav/plotbiomes/master/inst/doc/Whittaker_biomes_examples.html)
+#'
+#' or run
+#'
+#' `RShowDoc("Whittaker_biomes_examples", package = "plotbiomes")`
+#'
+#' GitHub page [here](https://github.com/valentinitnelav/plotbiomes)
+#'
+#' @author
+#' Valentin Stefan
+#'
+#' @md
+#' @docType package
+#' @name plotbiomes
+NULL
